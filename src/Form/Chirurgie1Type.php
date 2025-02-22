c2e6e09590872befa9a5af0fd8a3785d898811fb
iheb belaid <ihebbelaid3@gmail.com>
2025-02-22 14:54:05 +0100

    correction controle de saisie chirurgie

--- src/Form/Chirurgie1Type.php
+++ src/Form/Chirurgie1Type.php
@@ -14,28 +14,48 @@ class Chirurgie1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom_operation')
-            ->add('date_chirurgie', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('nom_docteur', null, [
-                'label' => 'Nom du Docteur',
+            ->add('nom_operation', null, [
+                'required' => false, // Désactive la validation HTML5
                 'attr' => [
                     'class' => 'form-control mb-1',
                 ],
             ])
-            ->add('nom_etablissement')
-            ->add('notes')
+            ->add('date_chirurgie', null, [
+                'widget' => 'single_text',
+                'required' => false, // Désactive la validation HTML5
+                'empty_data' => null, // Permet de retourner null si le champ est vide
+                'attr' => [
+                    'class' => 'form-control mb-1',
+                ],
+            ])
+            ->add('nom_docteur', null, [
+                'label' => 'Nom du Docteur',
+                'required' => false, // Désactive la validation HTML5
+                'attr' => [
+                    'class' => 'form-control mb-1',
+                ],
+            ])
+            ->add('nom_etablissement', null, [
+                'required' => false, // Désactive la validation HTML5
+                'attr' => [
+                    'class' => 'form-control mb-1',
+                ],
+            ])
+            ->add('notes', null, [
+                'required' => false, // Désactive la validation HTML5
+                'attr' => [
+                    'class' => 'form-control mb-1',
+                ],
+            ])
             ->add('patient', EntityType::class, [
                 'class' => Patient::class,
                 'choice_label' => 'prename', 
                 'label' => 'Patient',
+                'required' => false, // Désactive la validation HTML5
                 'attr' => [
                     'class' => 'form-control mb-1',
                 ],
-            ])
-            
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
